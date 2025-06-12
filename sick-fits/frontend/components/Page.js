ed5b34338cbdf741bc116a2f69508f3e4929ff05
codersadikbd <devsadikbd@gmail.com>
2025-06-12 22:55:50 +0600

    Refactor Page component; consolidate styles and improve structure with styled-components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 const GlobalStyles = createGlobalStyle`
@@ -19,10 +19,34 @@ const GlobalStyles = createGlobalStyle`
     --offWhite: #EDEDED;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0, 0, 0, 0.09);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
   body{
-    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
   }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
 `;
 
 export default function Page({ children, cool }) {
@@ -30,9 +54,7 @@ export default function Page({ children, cool }) {
     <div>
       <GlobalStyles />
       <Header />
-      <h2>I am the page component</h2>
-      <h3>{cool}</h3>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
