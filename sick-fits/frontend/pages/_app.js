9b26ee5fd589a1faa9540f627e805c2d5264fc46
codersadikbd <devsadikbd@gmail.com>
2025-06-12 23:23:39 +0600

    Refactor nProgress styles and improve document structure; enhance loading indicators and style management

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,12 @@
 import Page from '../components/Page';
+import 'nprogress/nprogress.css';
+import nProgress from 'nprogress';
+import Router from 'next/router';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
