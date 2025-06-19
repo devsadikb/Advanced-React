5200eaca2fedf013fca398136e1956396d146157
devsadikb <devsadikbd@gmail.com>
2025-06-19 22:54:18 +0600

    feat: enhance keystone configuration with server, database, and UI settings

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,7 +1,26 @@
 import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
 
 const databaseURL =
     process.env.DATABASE_URL || 'mongodb://localhost:27017/sadik';
 const sessionConfig = {
     MaxAge: 60 * 60 * 24 * 365,
 };
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        },
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+    },
+    lists: createSchema({
+        // schemas items will be added here
+    }),
+    ui: {
+        isAccessAllowed: () => true,
+    },
+});
