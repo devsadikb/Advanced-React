2e309be714e580df5fc2a40ab16da5ccc33ac072
devsadikb <devsadikbd@gmail.com>
2025-06-20 11:23:52 +0600

    feat: implement user authentication and enhance keystone configuration

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,26 +1,51 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+    withItemData,
+    statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 
 const databaseURL =
     process.env.DATABASE_URL || 'mongodb://localhost:27017/sadik';
 const sessionConfig = {
     MaxAge: 60 * 60 * 24 * 365,
+    secret: process.env.COOKIE_SECRET || 'a-very-secure-secret',
 };
-export default config({
-    server: {
-        cors: {
-            origin: [process.env.FRONTEND_URL],
-            credentials: true,
-        },
-    },
-    db: {
-        adapter: 'mongoose',
-        url: databaseURL,
-    },
-    lists: createSchema({
-        // schemas items will be added here
-    }),
-    ui: {
-        isAccessAllowed: () => true,
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
     },
 });
+export default withAuth(
+    config({
+        server: {
+            cors: {
+                origin: [process.env.FRONTEND_URL],
+                credentials: true,
+            },
+        },
+        db: {
+            adapter: 'mongoose',
+            url: databaseURL,
+        },
+        lists: createSchema({
+            // schemas items will be added here
+            User,
+        }),
+        ui: {
+            isAccessAllowed: ({ session }) => {
+                // Allow access to the UI if the user is signed in
+                console.log(session);
+                return !!session?.data;
+            },
+        },
+        session: withItemData(statelessSessions(sessionConfig), {
+            User: 'id',
+        }),
+    })
+);
--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,1 +1,12 @@
-export 
+import { text, password, relationship } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const User = list({
+    // access
+    // ui
+    fields: {
+        name: text({ isRequired: true }),
+        email: text({ isRequired: true, isUnique: true }),
+        password: password(),
+    },
+});
