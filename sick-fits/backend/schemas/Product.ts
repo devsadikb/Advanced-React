6af10dc6347384fcfc6a1a87cc5407b5934f6f1f
devsadikb <devsadikbd@gmail.com>
2025-06-21 20:30:23 +0600

    fix: format code for consistency in keystone and Product schema files

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -2,30 +2,30 @@ import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
-    fields: {
-        name: text({ isRequired: true }),
-        description: text({ ui: { displayMode: 'textarea' } }),
-        photo: relationship({
-            ref: 'ProductImage.product',
-            ui: {
-                displayMode: 'cards',
-                cardFields: ['image', 'altText'],
-                inlineCreate: { fields: ['image', 'altText'] },
-                inlineEdit: { fields: ['image', 'altText'] },
-            },
-        }),
-        status: select({
-            options: [
-                { label: 'draft', value: 'DRAFT' },
-                { label: 'available', value: 'AVAILABLE' },
-                { label: 'unavailable', value: 'UNAVAILABLE' },
-            ],
-            defaultValue: 'DRAFT',
-            ui: {
-                displayMode: 'segmented-control',
-                createView: { fieldMode: 'hidden' },
-            },
-        }),
-        price: integer(),
-    },
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({ ui: { displayMode: 'textarea' } }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    status: select({
+      options: [
+        { label: 'draft', value: 'DRAFT' },
+        { label: 'available', value: 'AVAILABLE' },
+        { label: 'unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+  },
 });
