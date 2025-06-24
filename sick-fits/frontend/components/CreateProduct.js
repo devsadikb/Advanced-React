00a484bae0dd3abcbd774a85a69e0dc0b7fecb2f
devsadikb <devsadikbd@gmail.com>
2025-06-24 13:28:16 +0600

    feat: add controlled input for product name in CreateProduct component

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 export default function CreateProduct() {
+  const [Name, setName] = useState('Sadik');
   return (
     <form>
       <label htmlFor="name">
         Name
-        <input type="text" id="name" name="name" placeholder="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="name"
+          value={Name}
+        />
       </label>
     </form>
   );
