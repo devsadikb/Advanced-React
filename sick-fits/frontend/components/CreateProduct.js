9d081fc0784ea41b247ac8f86d0377d508f456ac
codersadikbd <devsadikbd@gmail.com>
2025-07-08 19:43:24 +0600

    Add redirect after product creation and import necessary modules

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,8 +1,10 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -47,15 +49,19 @@ export default function CreateProduct() {
         console.log(inputs);
 
         // Pass variables in the function call
-        await createProduct({
+        const res = await createProduct({
           variables: {
             name: inputs.name,
             description: inputs.description,
             price: parseInt(inputs.price),
             image: inputs.image,
           },
+          refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
         });
         clearForm();
+        Router.push({
+          pathname: `/products/${res.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />
