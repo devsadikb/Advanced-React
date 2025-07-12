50b1c343db20a4ee1f9cce392400de69e8a446ed
codersadikbd <devsadikbd@gmail.com>
2025-07-12 17:43:01 +0600

    Implement Single Product component with GraphQL query for product details

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,19 +1,5 @@
-import { useQuery } from '@apollo/client';
-import gql from 'graphql-tag';
-
-const SINGLE_PRODUCT_QUERY = gql`
-  query {
-    product(where: { id: "68553b608b917e1e349b91cc" }) {
-      id
-      name
-      description
-      price
-    }
-  }
-`;
+import SingleProduct from '../../components/SingleProduct';
 
 export default function SingleProductPage({ query }) {
-  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY);
-  console.log({ data, error, loading });
-  return <p>Single Product Page {query.id}</p>;
+  return <SingleProduct id={query.id} />;
 }
