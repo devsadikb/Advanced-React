ef76b6b7782ef00b49bf63b29cc5c9c4751e04e8
codersadikbd <devsadikbd@gmail.com>
2025-07-12 17:06:46 +0600

    Update repository URL in package.json and implement Single Product Page with GraphQL query

--- sick-fits/frontend/pages/product/[id].js
+++ sick-fits/frontend/pages/product/[id].js
@@ -1,7 +1,19 @@
-export default function SingleProductPage() {
-  return (
-    <div>
-      <h1>Single Product Page</h1>
-    </div>
-  );
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query {
+    product(where: { id: "68553b608b917e1e349b91cc" }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function SingleProductPage({ query }) {
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY);
+  console.log({ data, error, loading });
+  return <p>Single Product Page {query.id}</p>;
 }
