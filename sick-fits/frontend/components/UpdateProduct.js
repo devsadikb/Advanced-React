4d4dcc36dc55230f38241c4ee3aec319585ae9e9
codersadikbd <devsadikbd@gmail.com>
2025-07-15 22:07:37 +0600

    Implement UpdateProduct component with GraphQL queries and update UpdatePage to pass product ID

--- sick-fits/frontend/components/UpdateProduct.js
+++ sick-fits/frontend/components/UpdateProduct.js
@@ -1,3 +1,43 @@
-export default function UpdateProduct() {
-  return <p>Update!</p>;
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { id: $id, name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+export default function UpdateProduct({ id }) {
+  const { data, loading, error } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+  const [
+    updateProduct,
+    { data: updateData, errror: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+  return <p>Update {id}!</p>;
 }
