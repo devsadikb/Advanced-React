56c3f389040c8e9955266eeba7b341b4e266bba0
wesbos <wes@wesbos.com>
2025-07-18 19:43:58 +0600

    Update logo text in Header component to 'SickFits'

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -36,7 +36,7 @@ export default function Header() {
     <HeaderStyles>
       <div className="bar">
         <Logo>
-          <Link href="/">wesbos</Link>
+          <Link href="/">SickFits</Link>
         </Logo>
         <Nav />
       </div>
